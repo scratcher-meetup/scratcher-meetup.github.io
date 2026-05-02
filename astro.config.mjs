9b8c8be2f3ed492769ce9040135bdee8a682d8cc
Kenichi Wakabayashi <kenichi@crssrds.jp>
2026-05-02 19:08:36 +0900

    外部リンクアイコン追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import rehypeExternalLinks from 'rehype-external-links';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
-  site: 'https://scratcher-meetup.github.io',
-  base: '/',
+  site: "https://scratcher-meetup.github.io",
+  base: "/",
   integrations: [sitemap()],
   markdown: {
     rehypePlugins: [
-      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+          content: { type: "text", value: " ↗" },
+        },
+      ],
     ],
   },
 });
